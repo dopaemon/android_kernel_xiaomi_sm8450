9a935d6a99f5240a126eed37f2a1d882fda27996
Vivek Goyal <vgoyal@redhat.com>
2021-10-12 09:23:07 -0400

    security: Return xattr name from security_dentry_init_security()
    Right now security_dentry_init_security() only supports single security
    label and is used by SELinux only. There are two users of this hook,
    namely ceph and nfs.
    
    NFS does not care about xattr name. Ceph hardcodes the xattr name to
    security.selinux (XATTR_NAME_SELINUX).
    
    I am making changes to fuse/virtiofs to send security label to virtiofsd
    and I need to send xattr name as well. I also hardcoded the name of
    xattr to security.selinux.
    
    Stephen Smalley suggested that it probably is a good idea to modify
    security_dentry_init_security() to also return name of xattr so that
    we can avoid this hardcoding in the callers.
    
    This patch adds a new parameter "const char **xattr_name" to
    security_dentry_init_security() and LSM puts the name of xattr
    too if caller asked for it (xattr_name != NULL).
    
    Signed-off-by: Vivek Goyal <vgoyal@redhat.com>
    Reviewed-by: Jeff Layton <jlayton@kernel.org>
    Reviewed-by: Christian Brauner <christian.brauner@ubuntu.com>
    Acked-by: James Morris <jamorris@linux.microsoft.com>
    [PM: fixed typos in the commit description]
    Signed-off-by: Paul Moore <paul@paul-moore.com>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -2840,7 +2840,8 @@ static void selinux_inode_free_security(struct inode *inode)
 }
 
 static int selinux_dentry_init_security(struct dentry *dentry, int mode,
-					const struct qstr *name, void **ctx,
+					const struct qstr *name,
+					const char **xattr_name, void **ctx,
 					u32 *ctxlen)
 {
 	u32 newsid;
@@ -2852,6 +2853,9 @@ static int selinux_dentry_init_security(struct dentry *dentry, int mode,
 					   &newsid);
 	if (rc)
 		return rc;
+
+	if (xattr_name)
+		*xattr_name = XATTR_NAME_SELINUX;
 
 	return security_sid_to_context(newsid, (char **)ctx,
 				       ctxlen);
